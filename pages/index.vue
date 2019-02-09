9b182d4cb3ecca0625e3fd24843b06c1fe556bae
nathakits <nathakit.tan@gmail.com>
2019-02-09 23:56:44 +0700

    update layout

--- pages/index.vue
+++ pages/index.vue
@@ -3,12 +3,18 @@
     h1 Material icons
 
     v-layout(column wrap)
-      div.py-3(v-for="(iconarray, index) in arrIcons" :id="iconarray.title")
+      div.py-3.fullWidth(
+        v-for="(iconarray, index) in arrIcons"
+        :id="iconarray.title"
+      )
         h2 {{ iconarray.title.charAt(0).toUpperCase() + iconarray.title.slice(1) }}
         v-layout(row wrap)
           template(v-for="(svgs, index) in iconarray.svgs")
-            v-flex(xs4 sm4)
-              p {{ svgs }}
+            template(v-if="!svgs.match(/_48px.svg|_18px.svg|_36px.svg|battery|signal/g, '')")
+              v-flex(xs3 sm3)
+                v-btn.mx-2(block)
+                  v-icon {{ svgs.replace(/_24px.svg/g, '').replace(/\bic_/g, '') }}
+                  span {{ svgs.replace(/_24px.svg/g, '').replace(/\bic_/g, '') }}
 
 
 </template>
@@ -17,7 +23,6 @@
 import Logo from '~/components/Logo.vue'
 import VuetifyLogo from '~/components/VuetifyLogo.vue'
 import arrIcons from '~/assets/material-icons.json'
-
 
 export default {
   data: () => {
@@ -28,6 +33,13 @@ export default {
   components: {
     Logo,
     VuetifyLogo
-  }
+  },
 }
+
 </script>
+
+<style lang="stylus" scoped>
+.fullWidth
+ width 100%
+</style>
+
